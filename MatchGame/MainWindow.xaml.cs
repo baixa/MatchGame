6ece1f6690cc8b679ca7a313363ba60c5d0ad155
Maksimchuk Ivan <baixa2003@gmail.com>
2023-06-10 20:06:31 +0300

    Добавьте файлы проекта.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<String> emoji = new List<String>()
+            {
+                "😊", "😂", "🤣", "😎", "❤️", "😍", "😒", "👌", "😘", "💕",
+                "😊", "😂", "🤣", "😎", "❤️", "😍", "😒", "👌", "😘", "💕",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(emoji.Count);
+                string nextEmoji = emoji[index];
+                textBlock.Text = nextEmoji;
+                emoji.RemoveAt(index);
+            }
+
+        }
+    }
+}
